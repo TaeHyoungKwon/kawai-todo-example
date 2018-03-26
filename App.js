22b78549beb79ce9dc410139d6e2c74c523549af
thkwon <TaeHYoungKwon@github.com>
2018-03-26 14:00:08 +0900

    App Loading

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions,Platform } from 'react-native';
+import { AppLoading } from "expo";
 import ToDo from "./ToDo.js"
 
 import { ScrollView } from 'react-native-gesture-handler';
@@ -9,12 +10,22 @@ const {height, width} = Dimensions.get("window");
 export default class App extends React.Component {
   
   state = {
-    newToDo: ""//새롭게 추가되는 할일 리스트
+    newToDo: "",//새롭게 추가되는 할일 리스트
+    loadedToDos: false, // 보여지는toDO
   };
+
+  //컴포넌트가 만들어지고 첫 렌더링을 마친후 실행되는 메소드
+  componentDidMount = () => {
+    this._loadToDos();
+  }
 
   render() {
 
-    const { newToDo } = this.state; //state 값으로 newToDo를 받는다.
+    const { newToDo, loadedToDos } = this.state; //state 값으로 newToDo를 받는다.
+
+    if (!loadedToDos) {
+      return <AppLoading/>;
+    }
 
     return (
       <View style={styles.container}>
@@ -40,6 +51,11 @@ export default class App extends React.Component {
   _controllNewToDo = text => {
     this.setState({
       newToDo: text
+    });
+  };
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
     });
   };
 }
